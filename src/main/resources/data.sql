2adff3772214d75b08f28c68b027ac06968e59ed
ckeel <ckeel@naver.com>
2022-10-12 15:34:06 +0900

    #10 - 테스트 데이터를 data.sql 로 추가
    테스트 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert
+into
+    article
+(id, content, created_at, created_by, hashtag, modified_at, modified_by, title)
+values
+    (default, 'new Content', now(), 'ckeel', '#spring', now(), 'ckeel', 'new title');
