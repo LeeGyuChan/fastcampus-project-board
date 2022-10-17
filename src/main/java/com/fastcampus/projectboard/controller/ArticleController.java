c072e5a31e9f3f6e780784aad3c5c7e147e39625
ckeel <ckeel@naver.com>
2022-10-17 17:16:47 +0900

    #23  - 게시판 검색 기능 구현
    게시판 검색 기능 추가
    게시판 검색 기능 구현 중 option seleted 설정이 index.th파일에 잘 적용이 되지 않았음 이 부분도 일단 강의 진도 
    나가기 위해 넘어감

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -30,16 +31,18 @@ public class ArticleController {
     @GetMapping
     public String articles(
             @RequestParam(required = false) SearchType searchType,
-            @RequestParam(required = false) String SearchValue,
+            @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ){
-        Page<ArticleResponse> articles = articleService.searchArticles(searchType,SearchValue, pageable).map(ArticleResponse::from);
-        List<Integer> barNumbers = paginationService.getPaginationBarNumber(pageable.getPageNumber(), articles.getTotalPages());
 
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType,searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumber(pageable.getPageNumber(), articles.getTotalPages());
 
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchTypes", SearchType.values());
+
         return "articles/index";
     }
 
