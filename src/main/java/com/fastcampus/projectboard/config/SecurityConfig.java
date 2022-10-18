1d3e5d1f98321b86c2b804bec5426ff3487142b4
ckeel <ckeel@naver.com>
2022-10-18 15:31:04 +0900

    #31 - 인증기능 구현
    인강 진행 빠르게 따라가기 위해 일부 구현만 하고
    넘어 가기로 함.

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -1,9 +1,19 @@
 package com.fastcampus.projectboard.config;
 
 
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.dto.security.BoardPrincipal;
+import com.fastcampus.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +21,49 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+//        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+//                .formLogin().and()
+//                .build();
+
+        return http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "articles/",
+                                "articles/search-hashtag"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .formLogin().and()
+                .logout()
+                    .logoutSuccessUrl("/")
+                    .and()
                 .build();
+    }
+
+/* 더이상 권장되지 않음
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+
+        //정적 파일 제외
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+*/
+
+    @Bean
+    public UserDetailsService userDetailsService (UserAccountRepository userAccountRepository){
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(()-> new UsernameNotFoundException("유저를 찾을 수 없습니다. -username" + username));
 
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
+
